9aad836b93b5fc94982cc41740ebb49a1060c7b6
Adityakumar22102003 <aditya.bcastudent22.15762@cimage.in>
2025-07-21 22:52:48 +0530

    retrying pushing

--- 15762-Aditya-Kumar/server/models/Reviews.js
+++
@@ -1,7 +0,0 @@
-const reviewSchema = new mongoose.Schema({
-    productId: String,
-    name: String,
-    email: String,
-    message: String,
-    createdAt: { type: Date, default: Date.now }
-});
